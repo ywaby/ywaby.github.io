5b74ba81263de237e9ddfdfcb2ec1636c7fc3f32
ywaby <ywaby@debian.ywaby@163.com>
2019-03-05 20:35:52 +0800

    mv docs

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
         ? './'
         : '/',
     // 输出文件目录
-    outputDir: 'dist',
+    outputDir: 'docs',
     // eslint-loader 是否在保存的时候检查
     lintOnSave: true,
     // use the full build with in-browser compiler?
